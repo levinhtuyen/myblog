348b7284027e71ae4ce811c5c72d9c004766a5b5
tuyen-go2joy <levinhtuyen210@gmail.com>
2024-10-09 12:41:02 +0700

    update page

---
+++ components/awesome/Project.vue
@@ -0,0 +1,135 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <!-- Projects -->
+    <div class="pb-4 md:pb-12">
+      <h2
+        class="text-center text-2xl font-bold text-foreground md:text-4xl lg:text-6xl"
+      >
+        Projects
+      </h2>
+    </div>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-8 mx-auto">
+        <div class="flex flex-wrap -m-4">
+          <div class="p-4 md:w-1/3">
+            <div
+              class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden"
+            >
+              <img
+                width="372"
+                height="211"
+                loading="lazy"
+                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                src="/images/project-1.webp"
+                alt="blog"
+              />
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  Web App
+                </h2>
+                <h1
+                  class="title-font text-lg font-medium text-gray-600 mb-3 line-clamp-1"
+                >
+                  Web Tool
+                </h1>
+                <p class="leading-relaxed mb-3 line-clamp-3">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Tempora expedita dicta totam aspernatur doloremque. Excepturi
+                  iste iusto eos enim reprehenderit nisi, accusamus delectus
+                  nihil quis facere in modi ratione libero!
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <button
+                    class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md shadow-cla-blue px-4 py-1 rounded-lg"
+                  >
+                    Learn more
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3">
+            <div
+              class="h-full rounded-xl shadow-cla-violate bg-gradient-to-r from-pink-50 to-red-50 overflow-hidden"
+            >
+              <img
+                loading="lazy"
+                width="372"
+                height="211"
+                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                src="/images/project-2.webp"
+                alt="blog"
+              />
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  WebAdmin
+                </h2>
+                <h1
+                  class="title-font text-lg font-medium text-gray-600 mb-3 line-clamp-1"
+                >
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </h1>
+                <p class="leading-relaxed mb-3 line-clamp-3">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Tempora expedita dicta totam aspernatur doloremque. Excepturi
+                  iste iusto eos enim reprehenderit nisi, accusamus delectus
+                  nihil quis facere in modi ratione libero!
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <button
+                    class="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg"
+                  >
+                    Learn more
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 md:w-1/3">
+            <div
+              class="h-full rounded-xl shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden"
+            >
+              <img
+                loading="lazy"
+                width="372"
+                height="211"
+                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                src="/images/project-3.webp"
+                alt="blog"
+              />
+              <div class="p-6">
+                <h2
+                  class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
+                >
+                  Website
+                </h2>
+                <h1
+                  class="title-font text-lg font-medium text-gray-600 mb-3 line-clamp-1"
+                >
+                  My portfolio and blog
+                </h1>
+                <p class="leading-relaxed mb-3 line-clamp-3">
+                  Lê Vĩnh Tuyến - Blog's của tôi - Để chút thương gửi vào trong
+                  gió, để chút buồn gửi vào trong tim...
+                </p>
+                <div class="flex items-center flex-wrap">
+                  <button
+                    class="bg-gradient-to-r from-fuchsia-300 to-pink-400 hover:scale-105 shadow-cla-blue px-4 py-1 rounded-lg"
+                  >
+                    see more
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
